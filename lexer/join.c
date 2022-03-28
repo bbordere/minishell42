3b1a553a7809a1fc0c86545190dfe2b177db57dc
Bastien <bastien.bordereau@gmail.com>
2022-03-29 00:22:13 +0200

    nearly working join method

--- lexer/join.c
+++ lexer/join.c
@@ -6,7 +6,7 @@
 /*   By: bbordere <bbordere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 16:08:45 by bbordere          #+#    #+#             */
-/*   Updated: 2022/03/28 17:08:54 by bbordere         ###   ########.fr       */
+/*   Updated: 2022/03/29 00:16:29 by bbordere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,16 @@ size_t	ft_count_join(t_token **tokens)
 	{
 		if (tokens[i + 1] && !ft_islimit(tokens[i]->type))
 		{
-			while (tokens[i + 1] && !ft_islimit(tokens[i]->type))
+			while (tokens[i] && !ft_islimit(tokens[i]->type))
 				i++;
 			nb++;
 		}
-		nb++;
-		i++;
+		else
+		{
+			nb++;
+			i++;
+		}
 	}
-	
 	return (nb);
 }
 
@@ -55,49 +57,46 @@ char	**ft_join(t_token **tokens)
 		return (NULL);
 	i = 0;
 	j = 0;
-	printf("%lu\n", nb);
-	temp = NULL;
-	while (tokens[i])
+	while (j < nb)
 	{
-		if (tokens[i + 1] && !ft_islimit(tokens[i]->type))
+		temp = NULL;
+		if (tokens[i] && !ft_islimit(tokens[i]->type))
 		{
-			while (tokens[i + 1] && !ft_islimit(tokens[i]->type))
-			{
-				temp = ft_strjoin(ft_strjoin(temp, tokens[i]->val), " ");
-				i++;
-			}
+			while (tokens[i] && !ft_islimit(tokens[i]->type))
+				temp = ft_strjoin(ft_strjoin(temp, tokens[i++]->val), " ");
+		temp[ft_strlen(temp) - 1] = '\0';
+		res[j++] = ft_strdup(temp);
+		free(temp);
 		}
 		else
-		{
-			temp = tokens[i]->val;
-			i++;
-		}
-		printf("%s\n", temp);
-		i++;
+			res[j++] = ft_strdup(tokens[i++]->val);
 	}
 	res[j] = NULL;
 	return (res);
 }
 
-int main(int argc, char **av)
-{
-	char *str = av[1];
-	char **tab;
-	t_token	**tabo;
+// int main(int argc, char **av)
+// {
+// 	char *str = av[1];
+// 	char **tab;
+// 	t_token	**tabo;
 
-	tab = ft_lexer(str);
-	tabo = ft_tokenize(tab);
+// 	tab = ft_lexer(str);
+// 	tabo = ft_tokenize(tab);
 
-	char **joined;
-	joined = ft_join(tabo);
-	int i = 0;
-	// printf("\t\t%lu\n", ft_block_count(str));
-	while (joined[i])
-	{
-		printf("%s\n", joined[i]);
-		i++;
-	}
-	ft_free(tab);
-	ft_free(tabo);
-	return 0;
-}
+// 	char **joined;
+// 	joined = ft_join(tabo);
+// 	int i = 0;
+// 	// printf("\t\t%lu\n", ft_block_count(str));
+// 	t_token	**te = ft_tokenize(joined);
+// 	while (joined[i])
+// 	{
+// 		ft_printf("%d %s\n", te[i]->type, te[i]->val);
+// 		i++;
+// 	}
+// 	ft_free(joined);
+// 	ft_free(te);
+// 	ft_free(tabo);
+// 	ft_free(tab);
+// 	return 0;
+// }
